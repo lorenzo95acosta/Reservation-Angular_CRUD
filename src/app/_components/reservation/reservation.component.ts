d3a675852b0cb5405bbac0eb4f204a8a42583dd8
lorenzo95acosta <lorenzo.a.gavacciuto@gmail.com>
2022-05-16 23:40:19 -0300

    Bootstrap + Table of reservations

---
+++ src/app/_components/reservation/reservation.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation',
+  templateUrl: './reservation.component.html',
+  styleUrls: ['./reservation.component.css']
+})
+export class ReservationComponent implements OnInit {
+
+  bookings: any[] = [
+    {name:"Lorenzo", cabin:"333", active: true, restaurant:"Equis", date:"17/05/2022 20:00", attendees: 5},
+    {name:"Jose", cabin:"795", active: true, restaurant:"Zeta", date:"17/05/2022 21:30", attendees: 2}
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReservationComponent } from './_components/reservation/reservation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule
