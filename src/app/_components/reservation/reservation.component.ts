0d82b28a0f4c7614d61474ebf146c15ffbe8eae5
lorenzo95acosta <lorenzo.a.gavacciuto@gmail.com>
2022-05-17 11:25:11 -0300

    Table responsive

--- src/app/_components/reservation/reservation.component.ts
+++ src/app/_components/reservation/reservation.component.ts
@@ -9,6 +9,30 @@ export class ReservationComponent implements OnInit {
 
   bookings: any[] = [
     {name:"Lorenzo", cabin:"333", active: true, restaurant:"Equis", date:"17/05/2022 20:00", attendees: 5},
+    {name:"Jose", cabin:"795", active: true, restaurant:"Zeta", date:"17/05/2022 21:30", attendees: 2},
+    {name:"Lorenzo", cabin:"333", active: true, restaurant:"Equis", date:"17/05/2022 20:00", attendees: 5},
+    {name:"Jose", cabin:"795", active: true, restaurant:"Zeta", date:"17/05/2022 21:30", attendees: 2},
+    {name:"Lorenzo", cabin:"333", active: true, restaurant:"Equis", date:"17/05/2022 20:00", attendees: 5},
+    {name:"Jose", cabin:"795", active: true, restaurant:"Zeta", date:"17/05/2022 21:30", attendees: 2},
+    {name:"Lorenzo", cabin:"333", active: true, restaurant:"Equis", date:"17/05/2022 20:00", attendees: 5},
+    {name:"Jose", cabin:"795", active: true, restaurant:"Zeta", date:"17/05/2022 21:30", attendees: 2},
+    {name:"Lorenzo", cabin:"333", active: true, restaurant:"Equis", date:"17/05/2022 20:00", attendees: 5},
+    {name:"Jose", cabin:"795", active: true, restaurant:"Zeta", date:"17/05/2022 21:30", attendees: 2},
+    {name:"Lorenzo", cabin:"333", active: true, restaurant:"Equis", date:"17/05/2022 20:00", attendees: 5},
+    {name:"Jose", cabin:"795", active: true, restaurant:"Zeta", date:"17/05/2022 21:30", attendees: 2},
+    {name:"Lorenzo", cabin:"333", active: true, restaurant:"Equis", date:"17/05/2022 20:00", attendees: 5},
+    {name:"Jose", cabin:"795", active: true, restaurant:"Zeta", date:"17/05/2022 21:30", attendees: 2},
+    {name:"Lorenzo", cabin:"333", active: true, restaurant:"Equis", date:"17/05/2022 20:00", attendees: 5},
+    {name:"Jose", cabin:"795", active: true, restaurant:"Zeta", date:"17/05/2022 21:30", attendees: 2},
+    {name:"Lorenzo", cabin:"333", active: true, restaurant:"Equis", date:"17/05/2022 20:00", attendees: 5},
+    {name:"Jose", cabin:"795", active: true, restaurant:"Zeta", date:"17/05/2022 21:30", attendees: 2},
+    {name:"Lorenzo", cabin:"333", active: true, restaurant:"Equis", date:"17/05/2022 20:00", attendees: 5},
+    {name:"Jose", cabin:"795", active: true, restaurant:"Zeta", date:"17/05/2022 21:30", attendees: 2},
+    {name:"Lorenzo", cabin:"333", active: true, restaurant:"Equis", date:"17/05/2022 20:00", attendees: 5},
+    {name:"Jose", cabin:"795", active: true, restaurant:"Zeta", date:"17/05/2022 21:30", attendees: 2},
+    {name:"Lorenzo", cabin:"333", active: true, restaurant:"Equis", date:"17/05/2022 20:00", attendees: 5},
+    {name:"Jose", cabin:"795", active: true, restaurant:"Zeta", date:"17/05/2022 21:30", attendees: 2},
+    {name:"Lorenzo", cabin:"333", active: true, restaurant:"Equis", date:"17/05/2022 20:00", attendees: 5},
     {name:"Jose", cabin:"795", active: true, restaurant:"Zeta", date:"17/05/2022 21:30", attendees: 2}
   ];
 
