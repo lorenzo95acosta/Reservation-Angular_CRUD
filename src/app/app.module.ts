974f9ac9b82f9db0bd593ad00d3dc1389313ccdd
lorenzo95acosta <lorenzo.a.gavacciuto@gmail.com>
2022-05-17 10:46:46 -0300

    fontawesome + angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ReservationComponent } from './_components/reservation/reservation.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
