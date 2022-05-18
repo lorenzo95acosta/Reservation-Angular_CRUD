50f9f7dcfb3119abef39935b975be2afce0ed07e
lorenzo95acosta <lorenzo.a.gavacciuto@gmail.com>
2022-05-18 11:52:01 -0300

    Backend_config(api_.net6).2

---
+++ reservation_back/reservation_back/Startup.cs
@@ -0,0 +1,41 @@
+namespace reservation_back
+{
+  public static class Startup
+  {
+    public static WebApplication InitializeApp(string[] args)
+    {
+      var builder = WebApplication.CreateBuilder(args);
+      ConfigureServices(builder);
+      //In this app variable, we configure the middlewares
+      var app = builder.Build();
+      Configure(app);
+      return app;
+    }
+
+    private static void ConfigureServices(WebApplicationBuilder builder)
+    {
+
+      builder.Services.AddControllers();
+      // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+      builder.Services.AddEndpointsApiExplorer();
+      builder.Services.AddSwaggerGen();
+    }
+
+    private static void Configure(WebApplication app)
+    {
+      // Configure the HTTP request pipeline.
+      if (app.Environment.IsDevelopment())
+      {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseAuthorization();
+
+      app.MapControllers();
+    }
+
+  }
+}
